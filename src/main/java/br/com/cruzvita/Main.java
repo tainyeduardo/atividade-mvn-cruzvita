3d386570549375062cda4588296df995a78731d4
Tainy Souza <tainy.eduardo@gmail.com>
2023-02-09 01:04:51 -0300

    Adicionando projeto Maven.

---
+++ src/main/java/br/com/cruzvita/Main.java
@@ -0,0 +1,7 @@
+package br.com.cruzvita;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Cruz Vita");
+    }
+}
---
+++ src/main/java/br/com/cruzvita/entities/Banco.java
@@ -0,0 +1,4 @@
+package br.com.cruzvita.entities;
+
+public class Banco {
+}
